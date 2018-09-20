a2477ac0bc9bdeccf49e022fdf3d20de88537ec1
wuyechun2018 <wuyechun2018@163.com>
2018-09-20 11:38:08 +0800

    拼音工具类添加多音字处理方法

--- bxs-common/src/main/java/com/bxs/common/utils/PinyinUtil.java
+++ bxs-common/src/main/java/com/bxs/common/utils/PinyinUtil.java
@@ -1,4 +1,9 @@
 package com.bxs.common.utils;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang3.RandomStringUtils;
 
 import net.sourceforge.pinyin4j.PinyinHelper;
@@ -9,6 +14,10 @@ import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombi
 public class PinyinUtil {
 
 	public static void main(String[] args) {
+		System.out.println(toPinyin("李长是市长"));
+		//支持多音字
+		System.out.println(converterToSpell("李长是市长"));
+		System.out.println(toPinyin("我在长安城里快乐的成长"));
 		System.out.println(toPinyin(clearNotChinese("前置机表名称（xxb用户下）")));
 		System.out.println(toFirstChar("我是中国人"));
 		System.out.println(toPinyin("张海洋"));
@@ -16,6 +25,110 @@ public class PinyinUtil {
 		
 		System.out.println(RandomStringUtils.randomAlphanumeric(16));;
 	}
+	
+	
+	  public static String converterToSpell(String chines) {    
+          StringBuffer pinyinName = new StringBuffer();    
+          char[] nameChar = chines.toCharArray();    
+          HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();    
+          defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);    
+          defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);    
+          for (int i = 0; i < nameChar.length; i++) {    
+              if (nameChar[i] > 128) {    
+                  try {    
+                      // 取得当前汉字的所有全拼    
+                      String[] strs = PinyinHelper.toHanyuPinyinStringArray(    
+                              nameChar[i], defaultFormat);    
+                      if (strs != null) {    
+                          for (int j = 0; j < strs.length; j++) {    
+                              pinyinName.append(strs[j]);    
+                              if (j != strs.length - 1) {    
+                                  pinyinName.append(",");    
+                              }    
+                          }    
+                      }    
+                  } catch (BadHanyuPinyinOutputFormatCombination e) {    
+                      e.printStackTrace();    
+                  }    
+              } else {    
+                  pinyinName.append(nameChar[i]);    
+              }    
+              pinyinName.append(" ");    
+          }    
+          return parseTheChineseByObject(discountTheChinese(pinyinName.toString()));    
+      }
+	  
+	  private static String parseTheChineseByObject(    
+              List<Map<String, Integer>> list) {    
+          Map<String, Integer> first = null; // 用于统计每一次,集合组合数据    
+          // 遍历每一组集合    
+          for (int i = 0; i < list.size(); i++) {    
+              // 每一组集合与上一次组合的Map    
+              Map<String, Integer> temp = new Hashtable<String, Integer>();    
+              // 第一次循环，first为空    
+              if (first != null) {    
+                  // 取出上次组合与此次集合的字符，并保存    
+                  for (String s : first.keySet()) {    
+                      for (String s1 : list.get(i).keySet()) {    
+                          String str = s + s1;    
+                          temp.put(str, 1);    
+                      }    
+                  }    
+                  // 清理上一次组合数据    
+                  if (temp != null && temp.size() > 0) {    
+                      first.clear();    
+                  }    
+              } else {    
+                  for (String s : list.get(i).keySet()) {    
+                      String str = s;    
+                      temp.put(str, 1);    
+                  }    
+              }    
+              // 保存组合数据以便下次循环使用    
+              if (temp != null && temp.size() > 0) {    
+                  first = temp;    
+              }    
+          }    
+          String returnStr = "";    
+          if (first != null) {    
+              // 遍历取出组合字符串    
+              for (String str : first.keySet()) {    
+                  returnStr += (str + ",");    
+              }    
+          }    
+          if (returnStr.length() > 0) {    
+              returnStr = returnStr.substring(0, returnStr.length() - 1);    
+          }    
+          return returnStr;    
+      }    
+      
+	  
+	  private static List<Map<String, Integer>> discountTheChinese(String theStr) {    
+          // 去除重复拼音后的拼音列表    
+          List<Map<String, Integer>> mapList = new ArrayList<Map<String, Integer>>();    
+          // 用于处理每个字的多音字，去掉重复    
+          Map<String, Integer> onlyOne = null;    
+          String[] firsts = theStr.split(" ");    
+          // 读出每个汉字的拼音    
+          for (String str : firsts) {    
+              onlyOne = new Hashtable<String, Integer>();    
+              String[] china = str.split(",");    
+              // 多音字处理    
+              for (String s : china) {    
+                  Integer count = onlyOne.get(s);    
+                  if (count == null) {    
+                      onlyOne.put(s, new Integer(1));    
+                  } else {    
+                      onlyOne.remove(s);    
+                      count++;    
+                      onlyOne.put(s, count);    
+                  }    
+              }    
+              mapList.add(onlyOne);    
+          }    
+          return mapList;    
+      }    
+      
 
 	/**
 	 * 
