5c5c32d076a1b556cb1af5d31e3f857f68d3f190
wuyechun2018 <wuyechun2018@163.com>
2019-03-08 16:28:34 +0800

    账单维护数据层

---
+++ bxs-manager/bxs-manager-jpa/src/main/java/org/bxs/jpa/BizAccountRepository.java
@@ -0,0 +1,9 @@
+package org.bxs.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bxs.pojo.jpa.ierp.BizAccount;
+
+public interface BizAccountRepository extends CrudRepository<BizAccount, String>{
+
+}
